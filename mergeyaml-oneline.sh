5d03487f1d69d3652b0689f0cb6cf877d1b116f0
Primate Lab <100426365+primatelab@users.noreply.github.com>
2023-03-30 18:22:52 +0100

    Create mergeyaml-oneline.sh

---
+++ mergeyaml-oneline.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# mergeyaml.sh has been minified and compressed for your convenience. It is now a "one-liner" you can use in your own scripts.
+
+echo 'H4sIAAAAAAACA91SyU7DMBC9z1cMxiINkjXAjVqtVFW9VeLCAVFolcROYpqlJGmRuvw7zi
+YqVH4AX8azvcXy9RX5JiPfK2PwRozfM/BteGAQDFw8gGpresQH6brS6QaFQqrSDb00x4VSK3RKWt
+JeUiQdZFwxHNugyWPwFZtE4yAqtN2coSMRna6HN2NSekfZNklciSqHwKv63hHbzUJ7CsOma0Ic6C
+DO7UhYD/SYTC4m4tUT+zvx+CZW77fD39BVrDP4WR236nwGkbV1ATK/iHnE3qokkljSkBM5Luik1D
+36IbQdJMkjeWJ4xhQaUHmmId2hCLti/0ZN4394F411cWIXrPcObalIURQhcg0niM//WceysOxWQy
+9Akn3TqKZd2kvDjyhIkINcWQjz9+8MkHu1Fm7oo078n8TKqS/W23y6mj7N55Pn2WiKZV5UtrbNzG
+c3u4a4Db1sA9+OTEEoNwMAAA==' | base64 -d | gzip -d | bash -s -- "$1" "$2"
