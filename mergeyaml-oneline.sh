22f244226f1d02e06f5a02062e3392e70bce2e5a
Primate Lab <100426365+primatelab@users.noreply.github.com>
2023-03-31 20:17:46 +0100

    fixed the --- dup bug

--- mergeyaml-oneline.sh
+++ mergeyaml-oneline.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-# mergeyaml.sh has been minified and compressed for your convenience. It is now a "one-liner" you can use in your own scripts.
+# yamlmerge.sh has been minified and compressed for your convenience. It is now a "one-liner" you can use in your own scripts.
 
-echo 'H4sIAAAAAAACA9VSTU/CQBC9z68Y141tTTajR9nQSAg3Ei8ejKikH9t2oR/YFkyg/He3hU
-KM+APcy2Tnzbw3bzLXV+TrnHyvSsCznR34Q8bvGQRDbmfLWmUrFCFSna3opXsOVCpEq6IP2kqKpY
-WM+8xlPKCQwVeiU4V2XCrTN0FLIrYFLXbjUqg2lK/T1JFhAYFXH5Hm0FUqL0TVQjpCWwVJYXDFmp
-6MydlIvHpieyce3sT8/Xbwk7NOVA6nNrcbKWIQGye/yIpLbE3vTBJJrGjAiSwHVFqpnnenDIIkeS
-z3Z4lIQ1jkCrINiggPyaO3A/BvvYrOqrjgtbdkUmWGooyQB7CHxHbwdERHjZlRNvq9uCSzwVgabR
-PJRERBgizkviHQfx9eVJS4QJ0jf2w35yFfuC7XtDxP1P6a6Xg+fppOR8+T4Riroqybda4/u9IUEu
-xCP7OGb1FV7oACAwAA' | base64 -d | gzip -d | bash -s -- $@
+echo 'H4sIAAAAAAACA9VSy07DMBC8+ysWY5EEyVo4UqsRVdVbJS4cEK8qDydxmzglSYvUpv+O4z
+SAoHwAvni1szuzs9rzMwyVxjCoMxK43p6EY8quKYnGzC1WjSzWwGPApljjg30eqWUMTo2vnHPEVI
+AJcSdM5ABlIfUpizCm5D1TuQQ3raShmIEjALqCDoMLH2O5Rb3Jc09AXJIoaI5Y2/dVMohBWkwl4M
+ooK02BpO3AR8XThD8GfHfFb5754uVy9JO2yaQmn42+nSuhJDXOftGVp/jawalA7GyOGKLjEZnXcu
+DdS4MACpaKw5dEokhcakmKLfAE+uTRXg/8Z7vcuuUn7A6uTKoqgFcJsIgcSPbtrI4ST0bYyA/aAs
+0O+0sygRUH4MjRARYaBvX3MSZlBUtQGtit3V4AbOn7TOGqn8kOzjU45lqdLp/bMbuCdj5dTO/m88
+n9bDyFuqyadqPVm99XZWC/wYgiH4kzhtsqAwAA' | base64 -d | gzip -d | bash -s -- $@
